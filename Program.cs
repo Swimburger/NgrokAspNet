4bd9097782ee0f3870148351955f6e41e9d11dd6
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-01-20 20:22:10 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using NgrokAspNet;
+using Twilio.TwiML;
+
+var builder = WebApplication.CreateBuilder(args);
+if (builder.Environment.IsDevelopment()) builder.Services.AddHostedService<NgrokService>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapPost("/voice", () =>
+{
+    var response = new VoiceResponse();
+    response.Say("Hello World!");
+    return Results.Text(response.ToString(), "text/xml");
+});
+app.MapPost("/message", () =>
+{
+    var response = new MessagingResponse();
+    response.Message("Hello World!");
+    return Results.Text(response.ToString(), "text/xml");
+});
+
+app.Run();
